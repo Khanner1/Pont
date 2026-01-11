a86a0186f128c3cbd441cedf6e041029fb2fc41f
Robert <wencessotelo700@gmail.com>
2026-01-11 09:58:06 -0800

    debug fixes

--- composeApp/src/iosMain/kotlin/com/example/pont/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/example/pont/MainViewController.kt
@@ -5,13 +5,6 @@ import com.example.pont.ui.theme.PontTheme
 import platform.Foundation.NSLog
 
 fun MainViewController() = ComposeUIViewController {
-    NSLog("🟢 MainViewController: ComposeUIViewController created")
-    try {
-        NSLog("🟢 About to call AppNavigation()")
-        AppNavigation()
-        NSLog("🟢 AppNavigation() returned successfully")
-    } catch (e: Exception) {
-        NSLog("🔴 ERROR in AppNavigation: ${e.message}")
-        throw e
-    }
+    NSLog("🟢 MainViewController: About to render AppNavigation")
+    AppNavigation()
 }
